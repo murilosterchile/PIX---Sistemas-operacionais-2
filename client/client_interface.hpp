2c5565744265cf4dc91eb83394a9603a92c793ac
J. Francisco <joaofhmunhoz@gmail.com>
2025-10-04 15:34:01 -0300

    interface cliente

---
+++ client/client_interface.hpp
@@ -0,0 +1,32 @@
+#ifndef CLIENT_INTERFACE_HPP
+#define CLIENT_INTERFACE_HPP
+
+#include <thread>
+#include <atomic>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+#include <string>
+
+class ClientProcessor;
+
+class ClientInterface{
+public:
+	ClientInterface(ClientProcessor& processor);
+	~ClientInterface();
+
+	void start();
+	void stop();
+
+private:
+	std::atomoc<bool> running;
+	std::thread input_thread;
+	std::thread output_thread;
+
+	ClientProcessor processor;
+
+	void input_loop();
+	void output_loop();
+};
+
+#endif
